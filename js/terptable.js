fcf8b8f589216dfefc3888ac39fda09e4fe5727b
Connor Wycherley <connorwych@googlemail.com>
2016-02-09 22:53:01 +0000

    Initial Set up of site
    Initial commit of all base code

---
+++ js/terptable.js
@@ -0,0 +1,3 @@
+var query = new google.visualization.Query(https://docs.google.com/spreadsheets/d/1yh6R0EBJLObcolu9Og-yCx-7S6HoD3PJED2TF89Wgfo/edit?usp=sharing);
+query.setQuery('select *');
+query.send(handleQueryResponse);
