099014d430ff28d3ea307c36e7e9850871edfbaf
Connor Wycherley <connorwych@googlemail.com>
2016-02-21 12:03:41 +0000

    Moved to Jade
    Moved HTML core to Jade and added in build scripts

---
+++ build_site.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Build HTML from Jade Files
+cd src/jade
+for f in *.jade;
+    do 
+#	echo "Processing $f" 	
+	jade $f --pretty
+done;
+rm head.html
+rm navigation.html
+rm footer.html
+mv *.html ../..
+
+#Build CSS file from BootStrap Less files
+cd ../less 
+lessc bootstrap.less > bootstrap.css 
+mv bootstrap.css ../../css
+echo -e '\n  completed build of bootstrap.css\n\n'
+lessc --clean-css bootstrap.less > bootstrap.min.css
+mv bootstrap.min.css ../../css
+echo -e '\n  completed build of bootstrap.min.css\n\n'
+
+
+
+
+
